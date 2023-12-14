6004a054f46ddfab4d51df4a593834a8dae8bc61
oriel <orielsilvajr@gmail.com>
2023-12-14 10:39:33 -0500

    ReFocus

---
+++ script.js
@@ -0,0 +1,30 @@
+// // function performance(btn) {
+// //     btn.innerText = 'Clicked'
+// // };
+
+// let array = ['cat', 'dog', 'monkey', 'kitchenSink', 'Waffles']
+
+// function random(arr) {
+
+// }
+
+// function performance(btn) {
+
+//     let buttonChanger = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (btn.innerText === 'First button')buttonChanger.push(btn.innerText = 'Clicked')
+
+//         else if (btn.innerText === 'Second button')  btn.innerText = 'Clicked'
+
+//         else (btn.innerText === 'Clicked')
+//             buttonChanger.push(btn.innerText = array[i])
+//         }
+//         return buttonChanger
+//     }
+
+
+// function hide(element) {
+//     element.remove();
+// }
+
