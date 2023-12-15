0799bb0a0c09604df720daaea2130e54e96c1cf6
oriel <orielsilvajr@gmail.com>
2023-12-14 20:58:42 -0500

    completed

--- script.js
+++ script.js
@@ -28,3 +28,29 @@
 //     element.remove();
 // }
 
+let sum = 0;
+let sum2 = 0;
+
+function sumFunc1(button) {
+    sum++;
+    button.innerHTML = `${sum} Hype`;
+    alert(`Ninja was liked: ${sum}`);
+};
+
+function sumFunc2(button) {
+    sum2++;
+    button.innerHTML = `${sum2} Hype`;
+    alert(`Ninja was liked: ${sum}`);
+};
+
+function statusFunc(button) {
+    if (button.innerHTML === 'Login') {
+        button.innerHTML = 'Logout'
+    } else {
+        button.innerHTML = 'Login'
+    }
+}
+
+function removalFunc(button) {
+    button.remove();
+}
